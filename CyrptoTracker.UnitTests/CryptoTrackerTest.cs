6e0b0786833f978d942dcbcb4f7b2a61755da58e
Tajinboxx <TajLonghurstsoft@gmail.com>
2021-11-22 17:03:51 +1300

    Added CyrptoTracker UItest

---
+++ CyrptoTracker.UnitTests/CryptoTrackerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Xamarin.UITests;
+
+namespace CyrptoTracker.UnitTests
+{
+    [TestFixture]
+    public class CryptoTrackerTest
+    {
+        [Test]
+        public void CryptoTracker_TestRefreshButton_ShouldRefreshPage()
+        {
+            //Arrange
+           
+            //Act
+          
+            //Assert
+        }
+
+    }
+}
